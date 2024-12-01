cea007a1a69d5e6a024ea95411201164836adef5
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2024-12-01 06:21:45 +0100

    [ADD] Template

---
+++ 2024/template/main.py
@@ -0,0 +1,22 @@
+##
+## PHYTON PROJECT, 2024
+## main.py
+## File description:
+## You know, I don t think there are good or bad descriptions,
+## for me, life is above all about functions...
+##
+
+lines = []
+while True:
+    line = input()
+    if line.lower() == 's':
+        break
+    lines.append(line)
+
+def main_silver(lines):
+    print(lines)
+
+def main_gold(lines):
+    print(lines)
+
+main_silver(lines)
